6434930d43be4bc90e060366192beb588c6ae581
takaki <t.takaki7255@gmail.com>
2022-08-01 21:04:44 +0900

    編集

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 import json
 import sys
@@ -10,3 +11,6 @@ app.config["JSON_AS_ASCII"] = False
 @app.route("/")
 def index():
     return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
